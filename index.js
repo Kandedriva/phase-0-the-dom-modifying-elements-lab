797041699f1ee478028eb30d6bff9aeb26e8fa7c
drissakande <drissakande@gmail.com>
2023-04-10 08:56:18 -0400

    Write code in index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const removeMain = document.getElementById("main");
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+
+newHeader.textContent = "<h1>Tyson is the champion</h1>";
